9a68d435953cf220284bf0e99efb06f60aa429cf
Saria Essid <saria.essid@esprit.tn>
2023-04-23 13:42:18 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react";
+import IdleTimer from "./IdleTimer";
+
+export default function App() {
+  const [isTimeout, setIsTimeout] = useState(false);
+  useEffect(() => {
+    const timer = new IdleTimer({
+      timeout: 10, //expire after 10 seconds
+      onTimeout: () => {
+        setIsTimeout(true);
+      },
+      onExpired: () => {
+        //do something if expired on load
+        setIsTimeout(true);
+      }
+    });
+
+    return () => {
+      timer.cleanUp();
+    };
+  }, []);
+
+  return <div>{isTimeout ? "Timeout" : "Active"}</div>;
+}
